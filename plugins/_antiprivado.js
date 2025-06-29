83a024e2fb1249427cff5593c65355c0b24e8d4c
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-29 17:43:48 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,13 +6,29 @@ export async function before(m, { conn, isOwner, isROwner }) {
   const botSettings = global.db.data.settings[this.user.jid] || {};
 
   if (botSettings.antiPrivate && !isOwner && !isROwner) {
-    await conn.reply(m.chat, '𝙃𝙤𝙡𝙖 👋, 𝙎𝙤𝙮 𝘼𝙡𝙚𝙚𝘽𝙤𝙩 🤖!
+    const mensaje = `𝙃𝙤𝙡𝙖 👋, 𝙎𝙤𝙮 𝘼𝙡𝙚𝙚𝘽𝙤𝙩 🤖!
 •𝙀𝙨𝙩𝙖 𝙋𝙧𝙤𝙝𝙞𝙗𝙞𝙙𝙤 𝙀𝙨𝙘𝙧𝙞𝙗𝙞𝙧𝙢𝙚 𝘼𝙡 𝙋𝙧𝙞𝙫𝙖𝙙𝙤 🚫.
 𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝘼𝙙𝙦𝙪𝙞𝙧𝙞𝙧 𝘼𝙡𝙚𝙚𝘽𝙤𝙩 🤖, 𝙈𝙚𝙣𝙨𝙪𝙖𝙡𝙢𝙚𝙣𝙩𝙚 𝙤 𝙥𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩𝙚𝙢𝙚𝙣𝙩𝙚 𝙡𝙤 𝙥𝙪𝙚𝙙𝙚𝙨 𝙝𝙖𝙘𝙚𝙧𝙡𝙤 𝙖𝙡 ✅: 
 
 https://wa.me/573206095607?text=.Adquirir
 
-𝙏𝙖𝙢𝙗𝙞𝙚𝙣 𝙨𝙚 𝙝𝙖𝙘𝙚𝙣 𝙗𝙤𝙩𝙨 𝙥𝙚𝙧𝙨𝙤𝙣𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨 🤖🖌️!.', m);
+𝙏𝙖𝙢𝙗𝙞𝙚𝙣 𝙨𝙚 𝙝𝙖𝙘𝙚𝙣 𝙗𝙤𝙩𝙨 𝙥𝙚𝙧𝙨𝙤𝙣𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨 🤖🖌️!`;
+
+    await conn.sendMessage(m.chat, {
+      text: mensaje,
+      contextInfo: {
+        externalAdReply: {
+          title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+          body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+          thumbnailUrl: 'https://qu.ax/JRCMQ.jpg',
+          sourceUrl: '',
+          mediaType: 1,
+          renderLargerThumbnail: false,
+          showAdAttribution: false
+        }
+      }
+    });
+
     await conn.updateBlockStatus(m.chat, 'block');
     console.log(`Usuario ${m.sender} bloqueado por contacto privado.`);
   }
